68fd14793a99cb07a64c6cbf0deb27261b974ed0
Matheus Fontes <matheusfontes1921@gmail.com>
2023-08-04 14:31:49 -0300

    feat: starting angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatTableModule} from '@angular/material/table';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { CadastroFormComponent } from './cadastro-form/cadastro-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CadastroFormComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        FormsModule,
+        MatTableModule,
+        BrowserAnimationsModule,
+    ]
 })
 export class AppModule { }
---
+++ src/app/cadastro-form/cadastro-form.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-form',
+  templateUrl: './cadastro-form.component.html',
+  styleUrls: ['./cadastro-form.component.scss'],
+  
+})
+export class CadastroFormComponent {
+   title = "cadastro-form";
+}
