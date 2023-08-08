8d390ea83405bd4ca4b801abcd1c1547f91daadf
Matheus Fontes <matheusfontes1921@gmail.com>
2023-08-08 12:43:02 -0300

    spring project

---
+++ backend/src/main/java/com/example/treinamentobrastechback/TreinamentoBrastechBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.treinamentobrastechback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoBrastechBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoBrastechBackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/treinamentobrastechback/TreinamentoBrastechBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treinamentobrastechback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoBrastechBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
